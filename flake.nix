2b73a67beaf5bf2f5840afff9bbb0cc126825c98
Cody P Schafer <dev@codyps.com>
2024-01-16 22:25:13 -0500

    use-case-hack = false

--- flake.nix
+++ flake.nix
@@ -110,6 +110,9 @@
                 home = "/Users/cody";
               };
               nixpkgs.hostPlatform = "x86_64-darwin";
+              nix.extraOptions = ''
+                use-case-hack = false
+              '';
             })
             ./nix-darwin/configuration.nix
             ./nix-darwin/linuxBuilder.nix

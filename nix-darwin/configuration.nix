07845c0302a0f39e257f122a48bf632d068e4224
Cody P Schafer <dev@codyps.com>
2024-04-10 18:35:14 -0400

    nix flake update & tweak darwin machine configs

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -34,15 +34,27 @@
   };
 
   nix.buildMachines = [{
-    hostName = "arnold";
-    system = "x86_64-linux";
+    hostName = "arnold-local";
+    systems = ["x86_64-linux" "aarch64-linux"];
+    maxJobs = 4;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+    protocol = "ssh-ng";
+  } {
+    hostName = "arnold-tailscale";
+    systems = ["x86_64-linux" "aarch64-linux"];
     maxJobs = 4;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
     protocol = "ssh-ng";
   }];
 
-  environment.etc."nix/ssh_config.d/linux-builder".text = ''
-    Host arnold
+  environment.etc."nix/ssh_config.d/arnold".text = ''
+    Host arnold-local
+      User nix
+      HostName 192.168.6.10
+      IdentityFile /etc/nix/keys/arnold_ed25519
+      UserKnownHostsFile /etc/nix/ssh_known_hosts.d/arnold
+
+    Host arnold-tailscale
       User nix
       HostName 100.96.147.23
       IdentityFile /etc/nix/keys/arnold_ed25519
@@ -51,9 +63,9 @@
 
   environment.etc."nix/ssh_known_hosts.d/arnold".text = ''
     100.96.147.23 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLLtofc9hAToGZfafrlv8/4tE5W0IARQ8nHs8DpBMSk
+    192.168.6.10 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLLtofc9hAToGZfafrlv8/4tE5W0IARQ8nHs8DpBMSk
   '';
 
-  ##ssh-ng://nix@100.96.147.23?ssh-key=/etc/nix/arnold_ed25519
   nix.settings.substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "builder-name:V3galmyKya9h+w11jUnPeq7bZ7h+G8mxl2F6rR0avPQ=" ];
   nix.extraOptions = ''

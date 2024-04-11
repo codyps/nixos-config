18168e657ec3b4335abb6eb3793d6ad5a11a5bc3
Cody P Schafer <dev@codyps.com>
2024-04-10 21:52:39 -0400

    disable arnold-tailscale

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -39,30 +39,17 @@
     maxJobs = 4;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
     protocol = "ssh-ng";
-  } {
-    hostName = "arnold-tailscale";
-    systems = ["x86_64-linux" "aarch64-linux"];
-    maxJobs = 4;
-    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-    protocol = "ssh-ng";
   }];
-
+  
   environment.etc."nix/ssh_config.d/arnold".text = ''
     Host arnold-local
       User nix
       HostName 192.168.6.10
       IdentityFile /etc/nix/keys/arnold_ed25519
       UserKnownHostsFile /etc/nix/ssh_known_hosts.d/arnold
-
-    Host arnold-tailscale
-      User nix
-      HostName 100.96.147.23
-      IdentityFile /etc/nix/keys/arnold_ed25519
-      UserKnownHostsFile /etc/nix/ssh_known_hosts.d/arnold
   '';
 
   environment.etc."nix/ssh_known_hosts.d/arnold".text = ''
-    100.96.147.23 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLLtofc9hAToGZfafrlv8/4tE5W0IARQ8nHs8DpBMSk
     192.168.6.10 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLLtofc9hAToGZfafrlv8/4tE5W0IARQ8nHs8DpBMSk
   '';
 

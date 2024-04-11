24d8ee6183b470ec19d63fc1be1c452a07f32b8c
Cody P Schafer <dev@codyps.com>
2024-04-10 21:18:52 -0400

    workaround zig + ncdu on darwin

--- nixpkgs/overlays/overlay.nix
+++ nixpkgs/overlays/overlay.nix
@@ -2,4 +2,14 @@ final: prev: {
   targo = with prev; (callPackage ./pkgs/targo.nix {
     inherit fetchFromGitHub rustPlatform;
   });
+  
+  # Workaround for nix+zig bug on darwin
+  # https://github.com/NixOS/nixpkgs/issues/287861#issuecomment-1962225863
+  ncdu = prev.ncdu.overrideAttrs {
+    __noChroot = prev.ncdu.stdenv.isDarwin;
+  };
+
+  zig_0_11 = prev.zig_0_11.overrideAttrs {
+    __noChroot = prev.zig_0_11.stdenv.isDarwin;
+  };
 }

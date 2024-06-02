de834f64608df5904ad7c4af19bf38001982c9db
Cody P Schafer <dev@codyps.com>
2024-06-01 21:19:12 -0400

    shell config with home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -114,15 +114,26 @@ in
 
   programs.zsh = {
     enable = true;
+    history = {
+      extended = true;
+      save = 10000;
+      size = 20000;
+      share = false;
+    };
+    initExtra = (builtins.readFile ./zshrc);
   };
 
   programs.bash = {
+    historyFileSize = -1;
+    historySize = -1;
+    historyFile = "\${HOME}/.bash_history_eternal";
+
     enable = true;
-    initExtra = ''
-      HISTTIMEFORMAT="%F %T "
-    '';
+    initExtra = (builtins.readFile ./bashrc);
+    profileExtra = (builtins.readFile ./profile.sh);
   };
 
+  programs.nix-index.enable = true;
   programs.fzf = {
     enable = true;
   };

6756be4db204736acbd5c793737877785baf2579
Cody P Schafer <dev@codyps.com>
2024-10-15 13:48:39 -0400

    wip

--- nixos/ssh-auth.nix
+++ nixos/ssh-auth.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
-{
+rec {
   authorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILO6B2Cx3SVmD65J9sJsmxhjZq/AGprzpRMcrqbCuu6Y cody@u3.bed.einic.org"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzOdeXt93UUseJFVdCdTysB+2xZN/Ig+jNGHjP8o8jZ cody@peyton"

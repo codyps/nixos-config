9e046c041c0738ef7f9d350f0148097ef6ffe65e
Cody P Schafer <dev@codyps.com>
2024-11-07 01:43:11 -0500

    more suspend blocking

--- nixos/ward/configuration.nix
+++ nixos/ward/configuration.nix
@@ -390,9 +390,21 @@ in
 
 
   # Enable the GNOME Desktop Environment.
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
-  services.xserver.desktopManager.gnome.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if (action.id == "org.freedesktop.login1.suspend" ||
+            action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.hibernate" ||
+            action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+        {
+            return polkit.Result.NO;
+        }
+    });
+  '';
+
 
 
   # Configure keymap in X11

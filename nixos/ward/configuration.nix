ffde9543e411c592ee06b64aae0531ede801a570
Cody P Schafer <dev@codyps.com>
2024-11-07 01:39:25 -0500

    ward: no autosuspend

--- nixos/ward/configuration.nix
+++ nixos/ward/configuration.nix
@@ -391,6 +391,7 @@ in
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
   services.xserver.desktopManager.gnome.enable = true;
 
 

b9715d5a672b4d3d42ab8f0d5b1e74cb12e3c9bb
Cody P Schafer <dev@codyps.com>
2025-01-09 15:52:26 -0500

    ward nix

--- hosts/ward/configuration.nix
+++ hosts/ward/configuration.nix
@@ -466,13 +466,24 @@ in
       }
     ];
   };
+
   services.tailscale.enable = true;
 
-  #system.copySystemConfiguration = true;
+  services.avahi = {
+    enable = true;
+    openFirewall = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
 
   zramSwap.enable = true;
 
   system.stateVersion = "23.11";
 }
-
-

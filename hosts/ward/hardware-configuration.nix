fd057df8ce72938eac0192dcd15cbd4e67c59817
Cody P Schafer <dev@codyps.com>
2025-01-12 21:20:33 -0500

    ward swap

--- hosts/ward/hardware-configuration.nix
+++ hosts/ward/hardware-configuration.nix
@@ -52,7 +52,9 @@
       options = [ "bind" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    "/dev/mapper/ward-swap"
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

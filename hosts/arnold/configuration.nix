ed2a2ca12b3def01c37b0d7c6c1e4b1a40caef2c
Cody P Schafer <dev@codyps.com>
2025-02-12 00:55:48 -0500

    wip

--- hosts/arnold/configuration.nix
+++ hosts/arnold/configuration.nix
@@ -688,7 +688,7 @@ in
       intel-vaapi-driver
       vaapiIntel
       libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      intel-compute-runtime-legacy1 # OpenCL filter support (hardware tonemapping and subtitle burn-in), Gen 8,9,11
       vpl-gpu-rt # QSV on 11th gen or newer
       intel-media-sdk # QSV up to 11th gen
     ];

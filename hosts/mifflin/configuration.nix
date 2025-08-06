256f269d3b6857bddd32d566eb6d0b1bd75b11ad
Cody P Schafer <dev@codyps.com>
2025-08-05 22:35:00 -0400

    wip

--- hosts/mifflin/configuration.nix
+++ hosts/mifflin/configuration.nix
@@ -53,7 +53,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;

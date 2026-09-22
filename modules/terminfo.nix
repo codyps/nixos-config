733592762c4a54bdd1407b89736a49656a338b46
Cody P Schafer <dev@codyps.com>
2026-09-21 20:44:39 -0400

    fix: ghostty

--- modules/terminfo.nix
+++ modules/terminfo.nix
@@ -1,12 +1,15 @@
 { lib, options, pkgs, ... }:
+let
+  ghostty = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
+in
 {
   # Support SSH sessions from Ghostty without installing the terminal emulator.
   config = lib.mkMerge [
     (lib.optionalAttrs (options ? environment.systemPackages) {
-      environment.systemPackages = [ pkgs.ghostty.terminfo ];
+      environment.systemPackages = [ ghostty.terminfo ];
     })
     (lib.optionalAttrs (options ? home.packages) {
-      home.packages = [ pkgs.ghostty.terminfo ];
+      home.packages = [ ghostty.terminfo ];
     })
   ];
 }
